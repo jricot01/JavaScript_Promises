3327c88c1fe728eaaf06df91bb2016e70a7775c4
jricot01 <jricot01@gmail.com>
2022-09-20 19:53:11 -0400

    Completed

--- app.js
+++ app.js
@@ -15,10 +15,32 @@ function getList() {
   });
 }
 
+// TODO: Select ELements
+let error = document.getElementById("error");
+let list = document.getElementById("list");
+
 // TODO: Handle the resolved or rejected states of the promise
+// getList().then((list) => {
+//   console.log(list);
+// });
+
+// getList().catch((error) => {
+//   console.error(error);
+// });
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+getList().then((list) => {
+  list.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+});
 
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+getList().catch((error) => {
+  console.error(error);
+  feedbackPara.textContent = error.message;
+});
